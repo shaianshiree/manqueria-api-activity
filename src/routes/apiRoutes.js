4295373b3cf00bb11a5a1cfccbdaaedbf1c4edd8
shaianshiree <manqueria.princessshaina@marsu.edu.ph>
2026-03-11 09:07:12 +0800

    added securing

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+// Import controllers here...
+const { protect, authorize } = require('../middleware/authMiddleware');
+
+// ANYONE can get dishes
+
+
+// ONLY Admins and Managers can create dishes
+
+
 const { 
     getAllDishes, 
     createDish, 
@@ -19,7 +29,7 @@ const {
 
 // Dish Routes
 router.get('/dishes', getAllDishes);
-router.post('/dishes', createDish);
+router.post('/dishes', protect, authorize('admin', 'manager'), createDish);
 router.get('/dishes/:id', getDishById);
 router.put('/dishes/:id', updateDish);
 router.delete('/dishes/:id', deleteDish);
